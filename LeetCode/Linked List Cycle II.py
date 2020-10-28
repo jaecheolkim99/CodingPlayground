c07fd9f603c56720c4140e33f72b511d444c7487
Jay Kim <kjc9911@naver.com>
2020-10-29 00:26:43 +0900

    Update Linked List Cycle II.py

--- LeetCode/Linked List Cycle II.py
+++ LeetCode/Linked List Cycle II.py
@@ -1,5 +1,24 @@
 """
+https://leetcode.com/submissions/detail/413764275/?from=/explore/challenge/card/october-leetcoding-challenge/562/week-4-october-22nd-october-28th/3509/
 
+[BEST]
+class Solution:
+    def detectCycle(self, head):
+        slow = fast = head
+        
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            
+            if slow == fast:
+                
+                slow2 = head
+
+                while slow != slow2:
+                    slow = slow.next
+                    slow2 = slow2.next
+
+                return slow
 
 """
 class Solution(object):
